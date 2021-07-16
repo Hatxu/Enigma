f0094ab11f4b219e742466793c20cc08a42878a6
Hatxu <52476615+Hatxu@users.noreply.github.com>
2021-07-16 07:56:58 -0500

    Final
    Cambios para que termine funcionando enigma

--- Funciones.py
+++ Funciones.py
@@ -3,12 +3,13 @@ from Reflector import Reflector
 from Plugboard import Plugboard
 
 # Valores            
-rotor1 =Rotor("IV",'A',"Rapida")
-rotor2 =Rotor("IV",'A',"Media")
-rotor3 =Rotor("IV",'A',"Lenta")
+rotor1 =Rotor("III",'A',"Rapida")
+rotor2 =Rotor("II",'A',"Media")
+rotor3 =Rotor("I",'A',"Lenta")
 reflector = Reflector("UKW-B")
 plugboardslist = [] #List to save all the plugs in it.
 listaenchufe = []
+contadorenchufe = 12
 Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 modelosR = ['I','II','III','IV','V']
 modelosE = ['UKW-A','UKW-B','UKW-C']
@@ -22,37 +23,54 @@ def plugValor(letra):
         return(sorted([valor.changeplug(letra) for valor in plugboardslist])[-1])
     else: 
         return(letra)
-       #return sorted([valor.changeplug(letra) for valor in plugboardslist])[-1] if sorted([valor.changeplug(letra) for valor in plugboardslist])[-1] != '0' else letra
-            
+       #return sorted([valor.changeplug(letra) for valor in plugboardslist])[-1] if sorted([valor.changeplug(letra) for valor in plugboardslist])[-1] != '0' else letra        
             
 def señalRes(valor):
-    """
-    print("VaI: ",valor)
-    print("VaP: ",plugValor(valor))
-    print("R1i: ",rotor1.letracual(rotor1.valorabc(rotor1.pasoletra( plugValor(valor) ))) )
-    print("R2i: ",rotor2.letracual(rotor2.valorabc(rotor2.pasoletra( rotor1.letracual(rotor1.valorabc(rotor1.pasoletra(plugValor(valor)))) ))))
-    print("R3i:  "+ rotor3.letracual(rotor3.valorabc(rotor3.pasoletra( rotor2.letracual(rotor2.valorabc(rotor2.pasoletra( rotor1.letracual(rotor1.valorabc(rotor1.pasoletra(plugValor(valor)))) ))) ))) )
-    print("Ref:  "+ reflector.pasoletra( rotor3.letracual(rotor3.valorabc(rotor3.pasoletra( rotor2.letracual(rotor2.valorabc(rotor2.pasoletra( rotor1.letracual(rotor1.valorabc(rotor1.pasoletra(plugValor(valor)))) ))) ))) ) )
-    print("R3o:  "+ rotor3.letracual(rotor3.valorabc(rotor3.pasoletra( reflector.pasoletra( rotor3.letracual(rotor3.valorabc(rotor3.pasoletra( rotor2.letracual(rotor2.valorabc(rotor2.pasoletra( rotor1.letracual(rotor1.valorabc(rotor1.pasoletra(plugValor(valor)))) ))) ))) ) ))) )
-    print("R2o:  "+ rotor2.letracual(rotor2.valorabc(rotor2.pasoletra( rotor3.letracual(rotor3.valorabc(rotor3.pasoletra( reflector.pasoletra( rotor3.letracual(rotor3.valorabc(rotor3.pasoletra( rotor2.letracual(rotor2.valorabc(rotor2.pasoletra( rotor1.letracual(rotor1.valorabc(rotor1.pasoletra(plugValor(valor)))) ))) ))) ) ))) ))) )
-    print("R1o:  "+ rotor1.letracual(rotor1.valorabc(rotor1.pasoletra( rotor2.letracual(rotor2.valorabc(rotor2.pasoletra( rotor3.letracual(rotor3.valorabc(rotor3.pasoletra( reflector.pasoletra( rotor3.letracual(rotor3.valorabc(rotor3.pasoletra( rotor2.letracual(rotor2.valorabc(rotor2.pasoletra( rotor1.letracual(rotor1.valorabc(rotor1.pasoletra(plugValor(valor)))) ))) ))) ) ))) ))) ))) )
-    """
-    return plugValor(rotor1.letracual(rotor1.valorabc(rotor1.pasoletra( rotor2.letracual(rotor2.valorabc(rotor2.pasoletra( rotor3.letracual(rotor3.valorabc(rotor3.pasoletra( reflector.pasoletra( rotor3.letracual(rotor3.valorabc(rotor3.pasoletra( rotor2.letracual(rotor2.valorabc(rotor2.pasoletra( rotor1.letracual(rotor1.valorabc(rotor1.pasoletra(plugValor(valor)))) ))) ))) ) ))) ))) ))) )
- 
+     
+   # print("VaI: ",valor)
+   # print("VaP: ",plugValor(valor))
+    
+    print("R1i: ",rotor1.pasoletra(rotor1.letracual(plugValor(valor))))
+   # print("Ori1: ",rotor1.valorabc(rotor1.pasoletra(rotor1.letracual(plugValor(valor)))))
+    print("R2i: ",rotor2.pasoletra(rotor2.letracual( rotor1.valorabc(rotor1.pasoletra(rotor1.letracual(plugValor(valor))))) ))
+   # print("Ori2: ",rotor2.valorabc(rotor2.pasoletra(rotor2.letracual( rotor1.valorabc(rotor1.pasoletra(rotor1.letracual(plugValor(valor))))) )))
+    
+    print("R3i: ", rotor3.pasoletra(rotor3.letracual(rotor2.valorabc(rotor2.pasoletra(rotor2.letracual( rotor1.valorabc(rotor1.pasoletra(rotor1.letracual(plugValor(valor))))) )))))
+   # print("Ori3: ", rotor3.valorabc(rotor3.pasoletra(rotor3.letracual(rotor2.valorabc(rotor2.pasoletra(rotor2.letracual( rotor1.valorabc(rotor1.pasoletra(rotor1.letracual(plugValor(valor)))))))))))
+    print("Ref:  "+ reflector.pasoletra(rotor3.valorabc(rotor3.pasoletra(rotor3.letracual(rotor2.valorabc(rotor2.pasoletra(rotor2.letracual( rotor1.valorabc(rotor1.pasoletra(rotor1.letracual(plugValor(valor))))))))))))
+
+    print("R3o: ",rotor3.regreso(rotor3.letracual(reflector.pasoletra(rotor3.valorabc(rotor3.pasoletra(rotor3.letracual(rotor2.valorabc(rotor2.pasoletra(rotor2.letracual( rotor1.valorabc(rotor1.pasoletra(rotor1.letracual(plugValor(valor))))))))))))))
+    print("R2o: ",rotor2.regreso(rotor2.letracual(rotor3.valorabc(rotor3.regreso(rotor3.letracual(reflector.pasoletra(rotor3.valorabc(rotor3.pasoletra(rotor3.letracual(rotor2.valorabc(rotor2.pasoletra(rotor2.letracual( rotor1.valorabc(rotor1.pasoletra(rotor1.letracual(plugValor(valor)))))))))))))))))
+    print("R2o: ",rotor1.regreso(rotor1.letracual(rotor2.valorabc(rotor2.regreso(rotor2.letracual(rotor3.valorabc(rotor3.regreso(rotor3.letracual(reflector.pasoletra(rotor3.valorabc(rotor3.pasoletra(rotor3.letracual(rotor2.valorabc(rotor2.pasoletra(rotor2.letracual( rotor1.valorabc(rotor1.pasoletra(rotor1.letracual(plugValor(valor))))))))))))))))))))
+
+
+    return plugValor(rotor1.valorabc(rotor1.regreso(rotor1.letracual(rotor2.valorabc(rotor2.regreso(rotor2.letracual(rotor3.valorabc(rotor3.regreso(rotor3.letracual(reflector.pasoletra(rotor3.valorabc(rotor3.pasoletra(rotor3.letracual(rotor2.valorabc(rotor2.pasoletra(rotor2.letracual( rotor1.valorabc(rotor1.pasoletra(rotor1.letracual(plugValor(valor)))))))))))))))))))))
+
+   
 def movimientoRotores(valor):
     lista=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    if rotor1.posicion == "Z":
-        rotor1.posicion = lista[0]
-        if rotor2.posicion == "Z":
-            rotor2.posicion = lista[0]
-            if rotor3.posicion == "Z":
-                 rotor3.posicion = lista[0]
-            else:     
-                rotor3.posicion =lista[lista.index(rotor3.posicion)+1] 
-        else:    
-            rotor2.posicion = lista[lista.index(rotor2.posicion)+1] 
+    if rotor1.posicion == rotor1.turnover:
+        rotor1.posicion = lista[lista.index(rotor1.posicion)+1]
+        if rotor2.posicion == 'Z':
+                rotor2.posicion = lista[0]
+        else:
+            if rotor2.posicion == rotor2.turnover:
+                if rotor3.posicion == 'Z':
+                    rotor3.posicion = lista[0]
+                else:
+                    rotor3.posicion =lista[lista.index(rotor3.posicion)+1]
+            rotor2.posicion = lista[lista.index(rotor2.posicion)+1]          
     else:
-        rotor1.posicion = lista[lista.index(rotor1.posicion)+1] 
+        if rotor1.posicion == 'Z':
+            rotor1.posicion = lista[0]
+        else:
+            rotor1.posicion = lista[lista.index(rotor1.posicion)+1]
+        if rotor2.posicion == rotor2.turnover:
+            rotor2.posicion = lista[lista.index(rotor2.posicion)+1]
+            if rotor3.posicion == 'Z':
+                rotor3.posicion = lista[0]
+
+            
     return señalRes(valor)
 
 # Funciones de Interfaz:
--- Rotor.py
+++ Rotor.py
@@ -4,6 +4,7 @@ class Rotor:
         self.modelo = modelo
         self.posicion = posicion
         self.velocidad = velocidad
+        self.turnover = ''
         self.lista = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
         self.diccio= {} #Diccionario Input
         self.version(modelo)
@@ -14,26 +15,37 @@ class Rotor:
     def version(self,modelo): #Modelo del rotor
         if modelo == 'I':
             self.diccio = {'A': 'E', 'B': 'K', 'C': 'M', 'D': 'F', 'E': 'L', 'F': 'G', 'G': 'D', 'H': 'Q', 'I': 'V', 'J': 'Z', 'K': 'N', 'L': 'T', 'M':'O', 'N': 'W', 'O': 'Y', 'P': 'H', 'Q': 'X', 'R': 'U', 'S': 'S', 'T': 'P', 'U': 'A', 'V': 'I', 'W': 'B', 'X': 'R', 'Y': 'C', 'Z': 'J'}
+            self.turnover = 'Q'
         elif modelo == 'II':
             self.diccio = {'A': 'A', 'B': 'J', 'C': 'D', 'D': 'K', 'E': 'S', 'F': 'I', 'G': 'R', 'H': 'U', 'I': 'X', 'J': 'B', 'K': 'L', 'L': 'H', 'M':'W', 'N': 'T', 'O': 'M', 'P': 'C', 'Q': 'Q', 'R': 'G', 'S': 'Z', 'T': 'N', 'U': 'P', 'V': 'Y', 'W': 'F', 'X': 'V', 'Y': 'O', 'Z': 'E'}
+            self.turnover = 'E'
         elif modelo == 'III':
             self.diccio = {'A': 'B', 'B': 'D', 'C': 'F', 'D': 'H', 'E': 'J', 'F': 'L', 'G': 'C', 'H': 'P', 'I': 'R', 'J': 'T', 'K': 'X', 'L': 'V', 'M':'Z', 'N': 'N', 'O': 'Y', 'P': 'E', 'Q': 'I', 'R': 'W', 'S': 'G', 'T': 'A', 'U': 'K', 'V': 'M', 'W': 'U', 'X': 'S', 'Y': 'Q', 'Z': 'O'}
+            self.turnover = 'V'
         elif modelo == 'IV':
             self.diccio = {'A': 'E', 'B': 'S', 'C': 'O', 'D': 'V', 'E': 'P', 'F': 'Z', 'G': 'J', 'H': 'A', 'I': 'Y', 'J': 'Q', 'K': 'U', 'L': 'I', 'M':'R', 'N': 'H', 'O': 'X', 'P': 'L', 'Q': 'N', 'R': 'F', 'S': 'T', 'T': 'G', 'U': 'K', 'V': 'D', 'W': 'C', 'X': 'M', 'Y': 'W', 'Z': 'B'}
+            self.turnover = 'J'
         elif modelo == 'V':
             self.diccio = {'A': 'V', 'B': 'Z', 'C': 'B', 'D': 'R', 'E': 'G', 'F': 'I', 'G': 'T', 'H': 'Y', 'I': 'U', 'J': 'P', 'K': 'S', 'L': 'D', 'M':'N', 'N': 'H', 'O': 'L', 'P': 'X', 'Q': 'A', 'R': 'W', 'S': 'M', 'T': 'J', 'U': 'Q', 'V': 'O', 'W': 'F', 'X': 'E', 'Y': 'C', 'Z': 'K'}
+            self.turnover = 'Z '
         else: 
             self.diccio=("No existe ese modelo")
-            
-    def valorabc(self,valor): #Es para sacar los valores de la tabla y obetener un resultado.
-        return self.lista.index(valor)+1    
-        
+
+    
     def pasoletra(self,letra): #devuelve la letra del diccionario
         return self.diccio.get(letra)
       
-    def letracual(self,valor): #En base a la posicion de la letra te dice cual sigue" 
-        resultado = self.lista.index(self.posicion)+valor-1
+    def letracual(self,valor): #En base a la posicion de la letra te dice cual sigue"
+        resultado = self.lista.index(self.posicion)+ self.lista.index(valor)
+        if resultado >= 26:
+            return self.lista[resultado-26]
+        return self.lista[resultado]
+
+    def valorabc(self,valor): #Es para sacar los valores de la tabla y obetener un resultado.
+        resultado = self.lista.index(valor) - self.lista.index(self.posicion)
         if resultado >= 26: 
             return self.lista[resultado-26]
-        return self.lista[resultado]    
-        
+        return self.lista[resultado]
+    
+    def regreso(self,letra): #Es para sacar los valores de la tabla y obetener un resultado.
+        return list(self.diccio.keys())[list(self.diccio.values()).index(letra)]
